55e0418ebf7f3cddb1bfdff8ad5618e95f910955
kihoon <caleb9510@gmail.com>
2024-11-13 18:07:53 +0900

    modify : modified-schema on dataschema.py

--- utils/dataSchema.py
+++ utils/dataSchema.py
@@ -5,12 +5,15 @@ from datetime import datetime
 # CivilComplaints 데이터 스키마
 
 class CivilComplaints(BaseModel):
+    Gu: str
     doc_id: int
     answer_url: str
     title: Optional[str]  # Make title Optional to allow None
     request_content: Optional[str]  # Make request_content Optional to allow None
     attached_file: List[Dict[str, Optional[int]]]  # 이미지 파일들을 리스트로 처리
     answer: Optional[str]  # Make answer Optional to allow None
+    answer_summary: Optional[str]  # Make answer_summary Optional to allow None
+    extraced_reference: Optional[str]  # Make extraced_reference Optional to allow None
     date: datetime
     dept: str
     state: str
